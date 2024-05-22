dee7a42feb6690a6f75a048b3b443a19d62e286a
0xHenriksson <jones51595@gmail.com>
2024-05-21 23:03:57 -0700

    v11 and v12

---
+++ playground/matmul_optimization/src/kernels.cuh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "kernels/sgemm_v01.cuh"
+#include "kernels/sgemm_v02.cuh"
+#include "kernels/sgemm_v03.cuh"
+#include "kernels/sgemm_v04.cuh"
+#include "kernels/sgemm_v05.cuh"
+#include "kernels/sgemm_v06.cuh"
+#include "kernels/sgemm_v07.cuh"
+#include "kernels/sgemm_v08.cuh"
+#include "kernels/sgemm_v09.cuh"
+#include "kernels/sgemm_v10.cuh"
